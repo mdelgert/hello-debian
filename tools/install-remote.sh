6506dc2325cc2099648525b9e293ad7166943a74
Matthew Elgert <mdelgert@yahoo.com>
2025-05-25 21:22:03 +0000

    Refactor install-remote.sh to create build-artifacts directory before changing into it

--- tools/install-remote.sh
+++ tools/install-remote.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd ../build-artifacts
+cd ../
+mkdir -p build-artifacts
+cd build-artifacts
 
 # wget https://github.com/mdelgert/hello-debian/releases/download/v1.0.0/hello-debian_0.0.2-1_amd64.deb
 # wget $(curl -s https://api.github.com/repos/mdelgert/hello-debian/releases/latest | grep "browser_download_url.*deb" | cut -d '"' -f 4)
